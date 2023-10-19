aa153ee5090712051a7b27a993c7ae8f22cabbd8
celianloisel <celianloisel@gmail.com>
2023-10-19 15:23:59 +0200

    Create party (without comment)

--- lib/widgets/bottom_navigation_bar_widget.dart
+++ lib/widgets/bottom_navigation_bar_widget.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:horse_factory/pages/home_page.dart';
+import 'package:horse_factory/pages/stable_page.dart';
 import 'package:horse_factory/pages/test_page.dart';
+
 import '../models/user.dart';
 
 class BottomNavigationBarWidget extends StatefulWidget {
   final User? user;
 
-  const BottomNavigationBarWidget({Key? key, required this.user}) : super(key: key);
+  const BottomNavigationBarWidget({Key? key, required this.user})
+      : super(key: key);
 
   @override
-  _BottomNavigationBarWidgetState createState() => _BottomNavigationBarWidgetState();
+  _BottomNavigationBarWidgetState createState() =>
+      _BottomNavigationBarWidgetState();
 }
 
 class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
@@ -20,15 +24,17 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
   void initState() {
     super.initState();
 
-    final user = widget.user ?? User(
-      email: 'example@email.com',
-      password: 'your_password',
-      profilePictureUrl: 'https://example.com/profile.png',
-      userName: 'your_username',
-    );
+    final user = widget.user ??
+        User(
+          email: 'example@email.com',
+          password: 'your_password',
+          profilePictureUrl: 'https://example.com/profile.png',
+          userName: 'your_username',
+        );
 
     _widgetOptions = <Widget>[
       HomePage(user: user),
+      StablePage(title: "Stable", user: user),
       const TestPage(title: "Test Page"),
     ];
   }
@@ -50,6 +56,10 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.house),
+            label: 'Stable',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.add),
